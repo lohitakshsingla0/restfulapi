6395b366f62bfcc832407e34f37faf0619ba191a
Lohitaksh Singla <lohitakshsingla0@gmail.com>
2022-06-27 19:09:43 +0200

    Difference between async await and promises

--- src/app.js
+++ src/app.js
@@ -7,10 +7,13 @@ const port = process.env.PORT ||8000;
 
 app.use(express.json());          //to use imcoming json data as object
 
-//create a new students
+//create a new students--------------promise
 app.post("/students", (req, res) => {
     console.log(req.body);
     const user = new Student(req.body)
+    
+    
+    
     user.save().then(() =>{
         res.status(201).send(user);
     }).catch((e) =>{
@@ -19,6 +22,19 @@ app.post("/students", (req, res) => {
     ;
     
 })
+
+//create a new students--------------async-await
+app.post("/students", async(req, res) => {
+    
+    try{
+        const user = new Student(req.body);
+        const createUser = await user.save();
+        res.status(201).send(createUser);
+    }catch(e){
+        res.status(400).send(e);
+    }  
+});
+
 
 app.listen(port, () => {
     console.log(`Connection is successfull at port: ${port} `);
